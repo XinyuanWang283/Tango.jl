0a96228075882c16b4c58c4018264cd4336a0fa4
Documenter.jl <documenter@juliadocs.github.io>
2024-04-16 12:51:36 +0000

    build based on eb9a2c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Tango","category":"page"},{"location":"#Tango","page":"Home","title":"Tango","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Tango.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Tango]","category":"page"}]
+}
